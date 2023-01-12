7ee3022bdbc145d6506be81264176f3647775ced
Nicholas Barone <47761143+nbarone1@users.noreply.github.com>
2023-01-12 14:50:53 -0600

    start

--- raw_transform.py
+++ raw_transform.py
@@ -1,14 +1,28 @@
 # Turn Raw into usable
 
+# Importing the pandas library and renaming it to pd.
 import pandas as pd
 
 # Select Data File
 import tkinter as tk
 from tkinter import filedialog
 
+# Importing the data file.
 root = tk.Tk()
 
 path = filedialog.askopenfilename()
 
 raw = pd.read_excel(path)
 
+# Get date list
+
+raw_dates = raw["%Calendar Date"]
+
+dates = []
+
+for i in range(0,len(raw_dates)):
+    if raw_dates[i] not in dates:
+        dates.append(raw_dates[i])
+
+print(len(dates))
+print(dates)
