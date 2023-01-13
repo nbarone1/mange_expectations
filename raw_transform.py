cb75755c64361d091fac3f5ea19add5543fe9e54
Nicholas Barone <47761143+nbarone1@users.noreply.github.com>
2023-01-13 08:25:58 -0600

    quick

--- raw_transform.py
+++ raw_transform.py
@@ -2,6 +2,7 @@
 
 # Importing the pandas library and renaming it to pd.
 import pandas as pd
+import numpy as np
 
 # Select Data File
 import tkinter as tk
@@ -16,13 +17,21 @@ raw = pd.read_excel(path)
 
 # Get date list
 
-raw_dates = raw["%Calendar Date"]
-
-dates = []
-
-for i in range(0,len(raw_dates)):
-    if raw_dates[i] not in dates:
-        dates.append(raw_dates[i])
+dates = np.unique(raw["%Calendar Date"])
 
 print(len(dates))
-print(dates)
+
+# get unique states
+
+ostates = np.unique(raw["Origin State"])
+dstates = np.unique(raw["Destination State"])
+
+rawstates = np.concatenate((ostates,dstates))
+
+states = np.unique(rawstates)
+print(len(states))
+
+# if date and origin/destination keep numbers
+
+for i in range(0,len(dates)):
+    
