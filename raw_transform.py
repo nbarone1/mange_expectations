11d57c6ae3a1f4f21bc79b3082b13c6e06a9eb40
Nicholas Barone <47761143+nbarone1@users.noreply.github.com>
2023-01-13 14:59:23 -0600

    list add

--- raw_transform.py
+++ raw_transform.py
@@ -18,6 +18,7 @@ raw = pd.read_excel(path)
 # Creating a dictionary with the key being the concatenation of the date and state and the value being
 # a list of the aggregated values.
 agg_date = {}
+dast = []
 
 # Creating a dictionary with the key being the concatenation of the date and the state. The value is a
 # list of the average shipper rate, average cost, load count, dat estimated rate, and 1.
@@ -31,6 +32,7 @@ for i in range(0,len(raw)):
         agg_date[concat][3] += raw.iloc[i].loc["DAT_EST_RATE"]
         agg_date[concat][4] += 1
     else:
+        dast.append(concat)
         agg_date[concat] = [raw.iloc[i].loc["Avg(Shipper_Rate)"],raw.iloc[i].loc["AVG COST"],raw.iloc[i].loc["Load Count"],raw.iloc[i].loc["DAT_EST_RATE"],1]
     if concat2 in agg_date:
         agg_date[concat2][0] += raw.iloc[i].loc["Avg(Shipper_Rate)"]
@@ -39,6 +41,7 @@ for i in range(0,len(raw)):
         agg_date[concat2][3] += raw.iloc[i].loc["DAT_EST_RATE"]
         agg_date[concat2][4] += 1
     else:
+        dast.append(concat2)
         agg_date[concat2] = [raw.iloc[i].loc["Avg(Shipper_Rate)"],raw.iloc[i].loc["AVG COST"],raw.iloc[i].loc["Load Count"],raw.iloc[i].loc["DAT_EST_RATE"],1]
 
 # Creating a dataframe from the dictionary.
